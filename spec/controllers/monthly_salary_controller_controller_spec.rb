bf212943f5588e2d5306ac5119adcfd592e43c30
Jophat Tamayo <>
2021-12-16 00:03:27 +0800

    get all collection and add test in rspec

--- spec/controllers/monthly_salary_controller_controller_spec.rb
+++ spec/controllers/monthly_salary_controller_controller_spec.rb
@@ -9,7 +9,21 @@ RSpec.describe 'MonthlySalaryControllerController', type: :request do
     let(:expected_result) { { 'employee_name' => 'Rin', 'gross_monthly_income' => '5000.00', 'monthly_income_tax' => '500.00', 'net_monthly_income' => '4500.00' } }
     context 'create data for employee salary' do
       it 'should add new entry' do
-        expect{ request }.to change(EmployeeSalary, :count).from(0).to(1)
+        expect { request }.to change(EmployeeSalary, :count).from(0).to(1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'POST #index' do
+    let(:request) { get '/monthly_salary' }
+    context 'show all data' do
+      let!(:employee_salary) do
+        { 'salary_computations': [create(:employee_salary)] }.to_json
+      end
+      it 'should return json data' do
+        request
+        expect(response.body).to eq(employee_salary)
         expect(response).to have_http_status(:ok)
       end
     end
